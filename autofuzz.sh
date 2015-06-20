581125a4b62f9b645125c5f0690a4b1763ab8791
Fatih ERDOGAN <g140910007@sakarya.edu.tr>
2015-06-21 02:19:15 +0300

    Create autofuzz.sh

---
+++ autofuzz.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# .m4a
+echo -n "Ses uzantisini gir: "
+read uzanti
+
+echo -n "Ses boyutunu gir: "
+read dosyaboyutu
+
+echo -n "Kac dosya olusturulsun?: "
+read dosyanum
+
+# dosyaboyutu=208
+ 
+offset=0
+
+# fuzz değeri
+fuzzval=255
+
+i=1
+while [ $i -lt $dosyanum ]
+do
+	cp ./Alarm.m4r ./$i$uzanti
+	./iosFuzzer $dosyaboyutu $offset $fuzzval ./$i$uzanti
+
+	let "offset += 1"
+	let "i += 1"
+done
